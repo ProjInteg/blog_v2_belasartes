8f7288180380efaeaafdf35c31a03cd936e79327
JPS-coder <87251421+JPS-coder@users.noreply.github.com>
2022-01-25 16:56:21 -0300

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('static/', css.blog_estilo, name = 'blog_estilo')
+    path('', static.blog_estilo, name = 'blog_estilo')
 ]
