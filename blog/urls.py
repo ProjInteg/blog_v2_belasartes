1f2e630c54e4fb5129070825adb645826617c5dd
JPS-coder <87251421+JPS-coder@users.noreply.github.com>
2022-01-25 15:50:40 -0300

    inserção path css

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('static/', css.blog_estilo, name = 'blog_estilo')
 ]
