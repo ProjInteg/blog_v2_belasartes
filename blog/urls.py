565282225c18d26f1bf96b2d72e261b2cb19595f
ProjInteg <92406801+ProjInteg@users.noreply.github.com>
2022-04-28 07:40:06 -0300

    HTML
    Mudança HTML

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('formulario', views.formulario, name='formulario'),
+    path('curso', views.curso, name='curso'),
     ]
