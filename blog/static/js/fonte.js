f58431f291519e421de0d226c7ace526b4d33588
ProjInteg <projint200@gmail.com>
2022-09-28 17:02:00 -0300

    Ajustes da acessibilidade

--- blog/static/js/fonte.js
+++ blog/static/js/fonte.js
@@ -1,5 +1,5 @@
     window.onload = function() {
-			var elementBody = document.querySelector('body');
+			var elementBody = document.querySelector('html');
 			var elementBtnIncreaseFont = document.getElementById('increase-font');
 			var elementBtnDecreaseFont = document.getElementById('decrease-font');
 			var fontSize = 100;
